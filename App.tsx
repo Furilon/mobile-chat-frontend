fb73f2afcfb25864a833f6c11542199a47056727
Mykyta Medvediev <arlekin8881@gmail.com>
2023-02-27 19:11:36 -0800

    Added simple socket test

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3000");
+socket.on("connect", () => {
+  console.log(`connected to server ${socket.id}`);
+});
 
 export default function App() {
   return (
